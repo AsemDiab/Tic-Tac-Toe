b7f1ab9ec7fc0f987275d3d6e7ba99f2070091a0
AsemDiab <asemhesham55@gmail.com>
2026-02-19 23:18:30 +0200

    added score and logic to decide who win

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+var Player = /** @class */ (function () {
+    function Player(name, symbol) {
+        this.socre = 0;
+        this.name = name;
+        this.symbol = symbol;
+    }
+    return Player;
+}());
 var Tic_Tac_Toe = /** @class */ (function () {
-    function Tic_Tac_Toe() {
+    function Tic_Tac_Toe(player1, player2) {
+        this.gameState = "playing";
+        this.turns = 0;
+        this.players = [player1, player2];
         this.board = [
             ["", "", ""],
             ["", "", ""],
             ["", "", ""],
         ];
         this.currentPlayer = "X";
+        this.resetGame();
+        this.updateTitle();
         this.init_actions();
+        this.initalizeResetButton();
     }
     Tic_Tac_Toe.prototype.init_actions = function () {
         var _this = this;
@@ -16,31 +30,63 @@ var Tic_Tac_Toe = /** @class */ (function () {
             var row = Math.floor(i / 3);
             var col = i % 3;
             cell.addEventListener("click", function () {
-                console.log("Clicked cell: ".concat(row, ", ").concat(col));
-                if (_this.board[row][col] === "") {
-                    _this.board[row][col] = _this.currentPlayer;
-                    //   cell.textContent = this.currentPlayer;
-                    cell.classList.add(_this.currentPlayer.toLowerCase() + "_cell");
-                    var move = { row: row, col: col, player: _this.currentPlayer };
-                    if (_this.isthereawinner(move)) {
-                        alert("".concat(_this.currentPlayer, " wins!"));
-                        _this.resetGame();
+                if (_this.gameState !== "playing")
+                    return;
+                _this.turns++;
+                _this.board[row][col] = _this.currentPlayer;
+                cell.classList.add(_this.currentPlayer.toLowerCase() + "_cell");
+                var move = { row: row, col: col, player: _this.currentPlayer };
+                if (_this.isthereawinner(move)) {
+                    _this.gameState = _this.currentPlayer === "X" ? "X wins" : "O wins";
+                    switch (_this.gameState) {
+                        case "X wins":
+                            _this.players[0].socre++;
+                            break;
+                        case "O wins":
+                            _this.players[1].socre++;
+                            break;
                     }
-                    else {
-                        _this.currentPlayer = _this.currentPlayer === "X" ? "O" : "X";
-                    }
+                    _this.updateScore();
                 }
+                else {
+                    _this.currentPlayer = _this.currentPlayer === "X" ? "O" : "X";
+                }
+                if (_this.turns === 9 && _this.gameState === "playing") {
+                    _this.gameState = "draw";
+                }
+                _this.updateTitle();
             });
             i++;
         });
     };
+    Tic_Tac_Toe.prototype.initalizeResetButton = function () {
+        var _this = this;
+        var resetButton = document.getElementById("resetButton");
+        if (resetButton) {
+            resetButton.addEventListener("click", function () {
+                _this.resetGame();
+            });
+        }
+    };
     Tic_Tac_Toe.prototype.isthereawinner = function (lastMove) {
         // Check rows
         if (this.board[lastMove.row].every(function (cell) { return cell === lastMove.player; })) {
+            this.board[lastMove.row].forEach(function (_, colIndex) {
+                var cell = document.getElementById("cell-".concat(lastMove.row * 3 + colIndex));
+                if (cell) {
+                    cell.classList.add("winning_cell");
+                }
+            });
             return true;
         }
         // Check columns
         if (this.board.every(function (row) { return row[lastMove.col] === lastMove.player; })) {
+            for (var rowIndex = 0; rowIndex < 3; rowIndex++) {
+                var cell = document.getElementById("cell-".concat(rowIndex * 3 + lastMove.col));
+                if (cell) {
+                    cell.classList.add("winning_cell");
+                }
+            }
             return true;
         }
         // Check diagonals
@@ -48,17 +94,30 @@ var Tic_Tac_Toe = /** @class */ (function () {
             this.board[0][0] === lastMove.player &&
             this.board[1][1] === lastMove.player &&
             this.board[2][2] === lastMove.player) {
+            this.board.forEach(function (_, index) {
+                var cell = document.getElementById("cell-".concat(index * 3 + index));
+                if (cell) {
+                    cell.classList.add("winning_cell");
+                }
+            });
             return true;
         }
         if (lastMove.row + lastMove.col === 2 &&
             this.board[0][2] === lastMove.player &&
             this.board[1][1] === lastMove.player &&
             this.board[2][0] === lastMove.player) {
+            this.board.forEach(function (_, index) {
+                var cell = document.getElementById("cell-".concat(index * 3 + (2 - index)));
+                if (cell) {
+                    cell.classList.add("winning_cell");
+                }
+            });
             return true;
         }
         return false;
     };
     Tic_Tac_Toe.prototype.resetGame = function () {
+        this.turns = 0;
         this.board = [
             ["", "", ""],
             ["", "", ""],
@@ -68,9 +127,53 @@ var Tic_Tac_Toe = /** @class */ (function () {
         var cells = document.querySelectorAll(".cell");
         cells.forEach(function (cell) {
             cell.textContent = "";
-            cell.classList.remove("x_cell", "o_cell");
+            cell.classList.remove("x_cell", "o_cell", "winning_cell");
         });
+        this.gameState = "playing";
+        this.updateTitle();
+    };
+    Tic_Tac_Toe.prototype.updateTitle = function () {
+        var title = document.getElementById("padTitle");
+        if (title) {
+            switch (this.gameState) {
+                case "X wins":
+                    title.textContent = "X wins!";
+                    break;
+                case "O wins":
+                    title.textContent = "O wins!";
+                    break;
+                case "draw":
+                    title.textContent = "It's a draw!";
+                    break;
+                default:
+                    title.textContent = "Tic Tac Toe";
+            }
+            if (this.gameState !== "playing") {
+                var blocker = document.getElementById("blocker");
+                if (blocker) {
+                    blocker.style.display = "flex";
+                }
+            }
+            else {
+                var blocker = document.getElementById("blocker");
+                if (blocker) {
+                    blocker.style.display = "none";
+                }
+            }
+        }
+    };
+    Tic_Tac_Toe.prototype.updateScore = function () {
+        var score1Element = document.getElementById("score1");
+        var score2Element = document.getElementById("score2");
+        if (score1Element) {
+            score1Element.textContent = "".concat(this.players[0].socre);
+        }
+        if (score2Element) {
+            score2Element.textContent = "".concat(this.players[1].socre);
+        }
     };
     return Tic_Tac_Toe;
 }());
-var game = new Tic_Tac_Toe();
+var player1 = new Player("Player 1", "X");
+var player2 = new Player("Player 2", "O");
+var game = new Tic_Tac_Toe(player1, player2);
--- script.ts
+++ script.ts
@@ -4,20 +4,35 @@ type Move = {
   player: string;
 };
 type GameStatus = "playing" | "X wins" | "O wins" | "draw";
+class Player {
+  name: string;
+  symbol: string;
+  socre: number = 0;
+  constructor(name: string, symbol: string) {
+    this.name = name;
+    this.symbol = symbol;
+  }
+}
 class Tic_Tac_Toe {
   private gameState: GameStatus = "playing";
   private board: string[][];
   private currentPlayer: string;
   private turns: number = 0;
-  constructor() {
+  private players: Player[];
+  constructor(player1: Player, player2: Player) {
+    this.players = [player1, player2];
     this.board = [
       ["", "", ""],
       ["", "", ""],
       ["", "", ""],
     ];
     this.currentPlayer = "X";
+    this.resetGame();
+    this.updateTitle();
     this.init_actions();
+    this.initalizeResetButton();
   }
+
   init_actions() {
     const cells = document.querySelectorAll(".cell");
     let i = 0;
@@ -25,35 +40,68 @@ class Tic_Tac_Toe {
       const row = Math.floor(i / 3);
       const col = i % 3;
       cell.addEventListener("click", () => {
+        if (this.gameState !== "playing") return;
+
         this.turns++;
-        console.log(`Clicked cell: ${row}, ${col}`);
-        if (this.board[row][col] === "") {
-          this.board[row][col] = this.currentPlayer;
-          //   cell.textContent = this.currentPlayer;
-          cell.classList.add(this.currentPlayer.toLowerCase() + "_cell");
-          const move: Move = { row, col, player: this.currentPlayer };
-          if (this.isthereawinner(move)) {
-            alert(`${this.currentPlayer} wins!`);
-            this.resetGame();
-          } else {
-            this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
+        this.board[row][col] = this.currentPlayer;
+        cell.classList.add(this.currentPlayer.toLowerCase() + "_cell");
+        const move: Move = { row, col, player: this.currentPlayer };
+
+        if (this.isthereawinner(move)) {
+          this.gameState = this.currentPlayer === "X" ? "X wins" : "O wins";
+          switch (this.gameState) {
+            case "X wins":
+              this.players[0].socre++;
+              break;
+            case "O wins":
+              this.players[1].socre++;
+              break;
           }
+          this.updateScore();
+        } else {
+          this.currentPlayer = this.currentPlayer === "X" ? "O" : "X";
         }
-        if (this.turns === 9) {
-          alert("It's a draw!");
-          this.resetGame();
+
+        if (this.turns === 9 && this.gameState === "playing") {
+          this.gameState = "draw";
         }
+        this.updateTitle();
       });
       i++;
     });
   }
+  initalizeResetButton() {
+    const resetButton = document.getElementById("resetButton");
+    if (resetButton) {
+      resetButton.addEventListener("click", () => {
+        this.resetGame();
+      });
+    }
+  }
+
   isthereawinner(lastMove: Move): boolean {
     // Check rows
     if (this.board[lastMove.row].every((cell) => cell === lastMove.player)) {
+      this.board[lastMove.row].forEach((_, colIndex) => {
+        const cell = document.getElementById(
+          `cell-${lastMove.row * 3 + colIndex}`,
+        );
+        if (cell) {
+          cell.classList.add("winning_cell");
+        }
+      });
       return true;
     }
     // Check columns
     if (this.board.every((row) => row[lastMove.col] === lastMove.player)) {
+      for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+        const cell = document.getElementById(
+          `cell-${rowIndex * 3 + lastMove.col}`,
+        );
+        if (cell) {
+          cell.classList.add("winning_cell");
+        }
+      }
       return true;
     }
     // Check diagonals
@@ -63,6 +111,12 @@ class Tic_Tac_Toe {
       this.board[1][1] === lastMove.player &&
       this.board[2][2] === lastMove.player
     ) {
+      this.board.forEach((_, index) => {
+        const cell = document.getElementById(`cell-${index * 3 + index}`);
+        if (cell) {
+          cell.classList.add("winning_cell");
+        }
+      });
       return true;
     }
     if (
@@ -71,12 +125,19 @@ class Tic_Tac_Toe {
       this.board[1][1] === lastMove.player &&
       this.board[2][0] === lastMove.player
     ) {
+      this.board.forEach((_, index) => {
+        const cell = document.getElementById(`cell-${index * 3 + (2 - index)}`);
+        if (cell) {
+          cell.classList.add("winning_cell");
+        }
+      });
       return true;
     }
     return false;
   }
 
   resetGame() {
+    this.turns = 0;
     this.board = [
       ["", "", ""],
       ["", "", ""],
@@ -86,9 +147,56 @@ class Tic_Tac_Toe {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
       cell.textContent = "";
-      cell.classList.remove("x_cell", "o_cell");
+      cell.classList.remove("x_cell", "o_cell", "winning_cell");
     });
+
+    this.gameState = "playing";
+    this.updateTitle();
+  }
+
+  updateTitle() {
+    const title = document.getElementById("padTitle");
+    if (title) {
+      switch (this.gameState) {
+        case "X wins":
+          title.textContent = "X wins!";
+          break;
+        case "O wins":
+          title.textContent = "O wins!";
+          break;
+        case "draw":
+          title.textContent = "It's a draw!";
+          break;
+        default:
+          title.textContent = "Tic Tac Toe";
+      }
+
+      if (this.gameState !== "playing") {
+        const blocker = document.getElementById("blocker");
+        if (blocker) {
+          blocker.style.display = "flex";
+        }
+      } else {
+        const blocker = document.getElementById("blocker");
+        if (blocker) {
+          blocker.style.display = "none";
+        }
+      }
+    }
+  }
+
+  updateScore() {
+    const score1Element = document.getElementById("score1");
+    const score2Element = document.getElementById("score2");
+    if (score1Element) {
+      score1Element.textContent = `${this.players[0].socre}`;
+    }
+    if (score2Element) {
+      score2Element.textContent = `${this.players[1].socre}`;
+    }
   }
 }
 
-const game = new Tic_Tac_Toe();
+const player1 = new Player("Player 1", "X");
+const player2 = new Player("Player 2", "O");
+const game = new Tic_Tac_Toe(player1, player2);
